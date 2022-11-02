2874f3624370acb220d353c387fb55c7d62fa77d
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2022-11-02 12:35:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ def continue_game_func():
 continue_game = "o"
 liste_choix = ["pierre", "feuille", "ciseau"]
 name_user = input("votre nom: ")
+point_ia = 0
+point_user = 0
 while continue_game != "n":
     player_1 = Player(name_user, liste_choix).choice_user_possi()
     player_ia = Ia(liste_choix).Ia_choice()
@@ -58,11 +60,19 @@ while continue_game != "n":
     print(control_game.is_winner())
     if control_game.is_winner() == 1:
       print("gagner")
+      point_user += 1
     elif control_game.is_winner() == 2:
       print("égalité")
     else:
       print("perdu")
+      point_ia += 1
     continue_game = continue_game_func()
+if point_user > point_ia:
+    print("Ia gagne avec une avance de ", point_user - point_ia, "et un score de = ", point_ia)
+elif point_user < point_ia:
+    print("User gagne avec une avance de ", point_ia - point_user, "et un score de = ", point_user)
+else:
+    print("égalité parfaite point ia = {}, point user = {}".format(point_ia, point_user))
 
 
 
