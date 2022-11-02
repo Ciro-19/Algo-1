3f972261db202172173edb9ed3e1d4be2b02a8ae
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2022-11-02 15:45:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from random import choice
-
 class MyGame:
     def __init__(self, choice_user, choice_ia):
         self.choice_user = choice_user
@@ -29,15 +27,40 @@ class Player:
         choice_user = -1
         while choice_user < 0 or choice_user > 2:
             choice_user = int(input("votre choix : "))
-        return choice_user
-        
+        return self.liste_choix[choice_user]
+
+
 class Ia:
-    def __init__(self, liste_choix):
+    def __init__(self, liste_choix, liste_coup):
         self.liste_choix = liste_choix
+        self.liste_coup = liste_coup
 
 
     def Ia_choice(self):
-        return choice(self.liste_choix)
+        if self.liste_coup[1] >= 2:
+            if self.liste_coup[0] == "feuille":
+                choix_ia = "ciseau"
+            elif self.liste_coup[0] == "pierre":
+                choix_ia = "feuille"
+            else:
+                choix_ia = "pierre" 
+        else:
+            coup_jouer = []
+            with open("dataUser.txt", "r") as file_data_open:
+                for row in file_data_open:
+                    coup_jouer.append(row)
+            coup_jouer = "".join(coup_jouer).split()
+            if len(coup_jouer) != 0:
+                if max(coup_jouer, key=coup_jouer.count) == "pierre":
+                    choix_ia = "feuille"
+                elif max(coup_jouer, key=coup_jouer.count) == "feuille":
+                    choix_ia = "ciseau"
+                else:
+                    choix_ia = "pierre"
+            else:
+                choix_ia = "feuille"
+        
+        return choix_ia
 
 
 def continue_game_func():
@@ -51,26 +74,47 @@ liste_choix = ["pierre", "feuille", "ciseau"]
 name_user = input("votre nom: ")
 point_ia = 0
 point_user = 0
+nbre_coup_pierre = 0
+nbre_coup_ciseau = 0
+nbre_coup_feuille = 0
+liste_coup = [0,0]
 while continue_game != "n":
     player_1 = Player(name_user, liste_choix).choice_user_possi()
-    player_ia = Ia(liste_choix).Ia_choice()
+    with open("dataUser.txt", "a") as file_data:
+        file_data.write(" " + player_1)
+    player_ia = Ia(liste_choix, liste_coup).Ia_choice()
     print("choix ia = {}".format(player_ia))
-    print("choix user = {}".format(liste_choix[player_1]))
-    control_game = MyGame(liste_choix[player_1], player_ia)
+    print("choix user = {}".format(player_1))
+    control_game = MyGame(player_1, player_ia)
     print(control_game.is_winner())
     if control_game.is_winner() == 1:
-      print("gagner")
-      point_user += 1
+        print("gagner")
+        point_user += 1
     elif control_game.is_winner() == 2:
-      print("égalité")
+        print("égalité")
     else:
-      print("perdu")
-      point_ia += 1
+        print("perdu")
+        point_ia += 1
+    if player_1 == "pierre":
+        nbre_coup_pierre += 1
+        nbre_coup_ciseau = 0
+        nbre_coup_feuille = 0
+        liste_coup = [player_1, nbre_coup_pierre]
+    elif player_1 == "feuille":
+        nbre_coup_feuille += 1
+        nbre_coup_ciseau = 0
+        nbre_coup_pierre = 0
+        liste_coup = [player_1, nbre_coup_feuille]
+    else:
+        nbre_coup_ciseau += 1
+        nbre_coup_pierre = 0
+        nbre_coup_feuille = 0
+        liste_coup = [player_1, nbre_coup_ciseau]
     continue_game = continue_game_func()
-if point_user > point_ia:
-    print("Ia gagne avec une avance de ", point_user - point_ia, "et un score de = ", point_ia)
-elif point_user < point_ia:
-    print("User gagne avec une avance de ", point_ia - point_user, "et un score de = ", point_user)
+if point_user < point_ia:
+    print("Ia gagne avec une avance de ", point_ia - point_user, "et un score de = ", point_ia)
+elif point_user > point_ia:
+    print("User gagne avec une avance de ", point_user - point_ia, "et un score de = ", point_user)
 else:
     print("égalité parfaite point ia = {}, point user = {}".format(point_ia, point_user))
 
