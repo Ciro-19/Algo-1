9ffe117ab426e607478e066a6a6b0bdbb53535bb
SimpleMg <117079680+SimpleMg@users.noreply.github.com>
2022-11-03 21:25:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,91 @@
 class MyGame:
-    def __init__(self:object, choice_user:str, choice_ia:str):
-        self.choice_user = choice_user
-        self.choice_ia = choice_ia
+    def __init__(self:object):
+        pass
     
 
-    def is_winner(self:object)->int:
-        if (self.choice_user == "feuille" and self.choice_ia == "pierre") or (self.choice_user == "pierre" and self.choice_ia == "ciseau") or (self.choice_user == "ciseau" and self.choice_ia == "feuille"):
+    def _is_winner(self:object, choice_user:str,choice_ia:str)->int:
+        if (choice_user == "feuille" and choice_ia == "pierre") or (choice_user == "pierre" and choice_ia == "ciseau") or (choice_user == "ciseau" and choice_ia == "feuille"):
             return 1
-        elif (self.choice_user == self.choice_ia):
+        elif (choice_user == choice_ia):
             return 2
         else:
             return 0
+
     
+    def _nbrePoint(self:object, point_user:int, point_ia:int):
+        if point_user < point_ia:
+            print("Ia gagne avec une avance de ", point_ia - point_user, "et un score de = ", point_ia)
+        elif point_user > point_ia:
+            print("User gagne avec une avance de ", point_user - point_ia, "et un score de = ", point_user)
+        else:
+            print("égalité parfaite point ia = {}, point user = {}".format(point_ia, point_user))
     
-    def continue_game_func(self:object)->str:
+
+    def _continue_game_func(self:object)->str:
         continue_game = ""
         while continue_game != "y" and continue_game != "n":
             continue_game = input("continuer ? (y/n)")
         return continue_game
+
+
+    def _calculPoint(self:object, choice_user, choice_ia, point_user, point_ia):
+        if self._is_winner(choice_user, choice_ia) == 1:
+            print("gagner")
+            point_user += 1
+        elif self._is_winner(choice_user, choice_ia) == 2:
+            print("égalité")
+        else:
+            print("perdu")
+            point_ia += 1
+        return [point_user, point_ia]
+
+
+    def _calculIa(self:object, choice_user,nbre_coup_pierre, nbre_coup_ciseau, nbre_coup_feuille, liste_coup):
+        if choice_user == "pierre":
+            nbre_coup_pierre += 1
+            nbre_coup_ciseau = 0
+            nbre_coup_feuille = 0
+            liste_coup = [choice_user, nbre_coup_pierre]
+        elif choice_user == "feuille":
+            nbre_coup_feuille += 1
+            nbre_coup_ciseau = 0
+            nbre_coup_pierre = 0
+            liste_coup = [choice_user, nbre_coup_feuille]
+        else:
+            nbre_coup_ciseau += 1
+            nbre_coup_pierre = 0
+            nbre_coup_feuille = 0
+            liste_coup = [choice_user, nbre_coup_ciseau]
+        return [liste_coup, nbre_coup_pierre, nbre_coup_ciseau, nbre_coup_feuille]
+
+
+    def start_game(self):
+        continue_game = "o"
+        liste_choix = ["pierre", "feuille", "ciseau"]
+        name_user = input("votre nom: ")
+        point_ia = 0
+        point_user = 0
+        liste_coup = [0,0]
+        nbre_coup_pierre = 0
+        nbre_coup_ciseau = 0
+        nbre_coup_feuille = 0
+        while continue_game != "n":
+            player_1 = Player(name_user, liste_choix).choice_user_possi()
+            with open("dataUser.txt", "a") as file_data:
+                file_data.write(" " + player_1)
+            result = self._calculIa(player_1, nbre_coup_pierre, nbre_coup_ciseau, nbre_coup_feuille, liste_coup)
+            liste_coup = result[0]
+            nbre_coup_pierre = result[1]
+            nbre_coup_ciseau = result[2]
+            nbre_coup_feuille = result[3]
+            player_ia = Ia(liste_choix, liste_coup).Ia_choice()
+            print("choix ia = {}".format(player_ia))
+            print("choix user = {}".format(player_1))
+            point_all = self._calculPoint(player_1, player_ia,point_user,point_ia)
+            point_user = point_all[0]
+            point_ia = point_all[1]
+            continue_game = self._continue_game_func()
+        self._nbrePoint(point_user, point_ia)
 
 
 class Player:
@@ -44,6 +112,7 @@ class Ia:
 
 
     def Ia_choice(self:object)->str:
+        print(self.liste_coup)
         if self.liste_coup[1] >= 2:
             if self.liste_coup[0] == "feuille":
                 choix_ia = "ciseau"
@@ -70,57 +139,10 @@ class Ia:
         return choix_ia
 
 
-continue_game = "o"
-liste_choix = ["pierre", "feuille", "ciseau"]
-name_user = input("votre nom: ")
-point_ia = 0
-point_user = 0
-nbre_coup_pierre = 0
-nbre_coup_ciseau = 0
-nbre_coup_feuille = 0
-liste_coup = [0,0]
-while continue_game != "n":
-    player_1 = Player(name_user, liste_choix).choice_user_possi()
-    with open("dataUser.txt", "a") as file_data:
-        file_data.write(" " + player_1)
-    player_ia = Ia(liste_choix, liste_coup).Ia_choice()
-    print("choix ia = {}".format(player_ia))
-    print("choix user = {}".format(player_1))
-    control_game = MyGame(player_1, player_ia)
-    print(control_game.is_winner())
-    if control_game.is_winner() == 1:
-        print("gagner")
-        point_user += 1
-    elif control_game.is_winner() == 2:
-        print("égalité")
-    else:
-        print("perdu")
-        point_ia += 1
-    if player_1 == "pierre":
-        nbre_coup_pierre += 1
-        nbre_coup_ciseau = 0
-        nbre_coup_feuille = 0
-        liste_coup = [player_1, nbre_coup_pierre]
-    elif player_1 == "feuille":
-        nbre_coup_feuille += 1
-        nbre_coup_ciseau = 0
-        nbre_coup_pierre = 0
-        liste_coup = [player_1, nbre_coup_feuille]
-    else:
-        nbre_coup_ciseau += 1
-        nbre_coup_pierre = 0
-        nbre_coup_feuille = 0
-        liste_coup = [player_1, nbre_coup_ciseau]
-    continue_game = control_game.continue_game_func()
-if point_user < point_ia:
-    print("Ia gagne avec une avance de ", point_ia - point_user, "et un score de = ", point_ia)
-elif point_user > point_ia:
-    print("User gagne avec une avance de ", point_user - point_ia, "et un score de = ", point_user)
-else:
-    print("égalité parfaite point ia = {}, point user = {}".format(point_ia, point_user))
 
 
 
 
-
+start_game = MyGame()
+start_game.start_game()
 
